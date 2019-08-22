b57e97e64dee806baadaf8c00e84844b5805420b
Kyle Melton <kalm42@gmail.com>
2019-08-22 17:11:25 -0600

    refactor: Hero css

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,6 +2,8 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
+import "./hero.scss"
+
 const Hero = () => {
   const data = useStaticQuery(graphql`
     query {
