8ae1835e6ab1b100ef75a8830514b4ec5f603040
Kyle Melton <kalm42@gmail.com>
2019-08-26 15:20:00 -0600

    feat: Styled footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import MobileMenu from "./mobile-menu/MobileMenu"
+import Footer from "./Footer";
 import "./layout.scss"
 
 const Layout = ({ children }) => {
@@ -20,57 +21,7 @@ const Layout = ({ children }) => {
     <>
       <MobileMenu siteTitle={data.site.siteMetadata.title} />
       <main>{children}</main>
-      <footer>
-        <h1>Contact Details</h1>
-        <p>© {new Date().getFullYear()}</p>
-        <p>(951)212-7174</p>
-        <p>me@kylemelton.dev</p>
-        <p>KyleMelton.dev</p>
-        <address>
-          2184 Channing Way #418
-          <br />
-          Idaho Falls, ID 83404
-        </address>
-        <div>{/* Social Media: GitHub */}</div>
-        <div>{/* Social Media: Facebook */}</div>
-        <div>{/* Social Media: LinkedIn */}</div>
-        <div>{/* Social Media: Twitter */}</div>
-        <div>
-          <div>
-            Social Icons made by
-            <a
-              href="https://www.flaticon.com/authors/dave-gandy"
-              title="Dave Gandy"
-            >
-              Dave Gandy
-            </a>
-            from{" "}
-            <a href="https://www.flaticon.com/" title="Flaticon">
-              www.flaticon.com
-            </a>
-          </div>
-          <div>
-            and
-            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
-              Freepik
-            </a>
-            from{" "}
-            <a href="https://www.flaticon.com/" title="Flaticon">
-              www.flaticon.com
-            </a>
-          </div>
-          <div>
-            Star Icon made by <a
-              href="https://www.flaticon.com/authors/smashicons"
-              title="Smashicons">Smashicons
-            </a> from{" "}
-            <a
-              href="https://www.flaticon.com/"
-              title="Flaticon">www.flaticon.com
-            </a>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </>
   )
 }
